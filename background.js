d32dcd331352dbb30c0536c2e49ab9956e8773dd
ParisaDarkhal <p.darkhal@gmail.com>
2024-09-11 16:10:00 -0400

    notification added

--- background.js
+++ background.js
@@ -7,9 +7,19 @@ chrome.alarms.onAlarm.addListener((alarm) => {
     chrome.storage.local.get(["timer", "isRunning"], (res) => {
       if (res.isRunning) {
         let timer = res.timer + 1;
-        // console.log(timer);
+        let isRunning = true;
+        if (timer === 25 * 60) {
+          this.registration.showNotification("Time's Up Note", {
+            body: "25 minutes is passed!",
+            icon: "icon.png",
+          });
+
+          timer = 0;
+          isRunning = false;
+        }
         chrome.storage.local.set({
           timer,
+          isRunning,
         });
       }
     });
@@ -21,4 +31,14 @@ chrome.storage.local.get(["timer", "isRunning"], (res) => {
     timer: "timer" in res ? res.timer : 0,
     isRunning: "isRunning" in res ? res.isRunning : false,
   });
+});
+
+chrome.permissions.contains({ permissions: ["notifications"] }, (result) => {
+  if (result) {
+    console.log("Notifications allowed!");
+    // Your notification creation code here
+  } else {
+    console.log("Notifications not allowed.");
+    // Handle permission request
+  }
 });
